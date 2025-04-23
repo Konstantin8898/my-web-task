f24797c993310b6e0243e7f35e92183f615034d5
Konstantin8898 <36132967+Konstantin8898@users.noreply.github.com>
2025-04-23 10:04:26 +0200

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?=
-$title = "PHP";
-$page_title = "general page";
-$content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, nulla? 
-Soluta placeat voluptates labore architecto est, voluptatum recusandae facere natus! Quo 
-ratione quas aperiam animi magni culpa aliquam accusamus veniam?";
-include("components/layout.php");
-?>
